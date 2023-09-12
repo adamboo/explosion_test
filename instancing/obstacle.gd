5d6bd78f0039fb3dfc44224f508c7a2038050d93
adamboo <61476771+adamboo@users.noreply.github.com>
2023-09-12 19:12:03 +0700

    Update obstacle.gd

--- instancing/obstacle.gd
+++ instancing/obstacle.gd
@@ -13,16 +13,23 @@ func _physics_process(delta):
 	pass
 
 
+func remap(value, InputA, InputB, OutputA, OutputB):
+	
+	return(value - InputA) / (InputB - InputA) * (OutputB - OutputA) + OutputA
+
+
 func _on_Area2D_area_entered(area):
 	var ball_position = area.global_position
-	var explosion_range = 100
-	var full_force = 500
-	var distance_to_ball =  global_position - ball_position
-	explosion_force = distance_to_ball / explosion_range * full_force
+	var dist_to_ball := global_position.distance_to(area.global_position)
+	var explosion_range := 200
+	dist_to_ball = clamp(dist_to_ball, 0, explosion_range)
+	var full_force := 500
+	var force = remap(dist_to_ball, 0, explosion_range, full_force, 0)
+	var dir_to_ball := global_position.direction_to(area.global_position)
+	explosion_force = dir_to_ball * force * -1
 	linear_velocity += explosion_force
 	print("ball position " + str(ball_position))
 	print("obs position " + str(global_position))
-	print("distance to ball " + str(distance_to_ball))
 	print("explosive force " + str(explosion_force))
 	
 	pass # Replace with function body.
